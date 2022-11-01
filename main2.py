9c105f882147c202112a07c602d4782f0944bcff
吳俊逸 <jim30205@gmail.com>
2022-11-01 10:58:55 +0800

    建立class的propertuy及常數

--- main2.py
+++ main2.py
@@ -1,11 +1,15 @@
 import tkinter as tk
 class ColorCanvas(tk.Canvas):
+    #建立Class的propertuy
+    #建立一個Class內建的常數
+    ON =  True
+    OFF = False
     def __init__(self,parent,rec_color,**kwargs):
         self.width = kwargs['width']
         self.height = kwargs['height']
         super().__init__(parent,**kwargs)
         self.rec_color = rec_color
-        self.__state = False
+        self.__state = ColorCanvas.OFF
         self.space = self.width / 7
         rec_width = self.width  - 2 * self.space 
         rec_height = self.height - 2 * self.space   
@@ -33,7 +37,7 @@ class Window(tk.Tk):
         # red.create_oval(x,y,x+10,y+10,fill="white",outline="white")
         red.bind('<ButtonRelease-1>',self.mouse_click)
         red.grid(row=0, column=0)
-        red.state = True
+        red.state = ColorCanvas.ON
         print(f"red狀態:{red.state}")
 
         green = ColorCanvas(self,"green",width=100,height=100)
