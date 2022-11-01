502172f9d3c65c5fa37007694bc895eb19146e80
吳俊逸 <jim30205@gmail.com>
2022-11-01 13:53:43 +0800

    點擊按鈕更換圓點位置

--- main2.py
+++ main2.py
@@ -11,9 +11,9 @@ class ColorCanvas(tk.Canvas):
         self.rec_color = rec_color
         self.__state = ColorCanvas.OFF
         self.space = self.width / 7
-        rec_width = self.width  - 2 * self.space 
-        rec_height = self.height - 2 * self.space   
+           
         self.create_rectangle(self.space, self.space, self.width - self.space, self.height - self.space,fill=self.rec_color)
+    
     @property
     def state(self):
         return self.__state
@@ -26,8 +26,7 @@ class ColorCanvas(tk.Canvas):
             #多加小圓點
             print("多加小圓點")
             rec_width = self.width  - 2 * self.space 
-            rec_height = self.height - 2 * self.space
-
+            rec_height = self.height - 2 * self.space            
             cir_width = rec_width / 5
             cir_height = rec_height / 5
             cir_start_x = self.space + cir_width / 2
@@ -35,38 +34,44 @@ class ColorCanvas(tk.Canvas):
             cir_start_y = self.space + rec_height * 5 / 7
             cir_end_y  =  cir_start_y + cir_height
             self.create_oval(cir_start_x,cir_start_y,cir_end_x,cir_end_y,fill='white',outline='white')
+       
 
 
 class Window(tk.Tk):
+    selected_convas = None
+    @classmethod
+    def get_select_convas(cls):
+        return cls.selected_convas
+
+    @classmethod
+    def set_select_convas(cls,convas):
+        if cls.selected_convas is not None:
+            cls.selected_convas.state = ColorCanvas.OFF   
+        cls.selected_convas = convas
+        cls.selected_convas.state = ColorCanvas.ON
+
     def __init__(self):
-        super().__init__()
+        super().__init__()           
         red = ColorCanvas(self,"red",width=100,height=100)
-        # x=70*1/5
-        # y=70*4/6
-        # width=10
-        # red.create_oval(x,y,x+10,y+10,fill="white",outline="white")
         red.bind('<ButtonRelease-1>',self.mouse_click)
-        red.grid(row=0, column=0)
-        red.state = ColorCanvas.ON
-        
-
+        red.grid(row=0, column=0)               
         green = ColorCanvas(self,"green",width=100,height=100)
-        green.grid(row=0, column=1)
-        green.state = ColorCanvas.ON
-
+        green.bind('<ButtonRelease-1>',self.mouse_click)        
+        green.grid(row=0, column=1)        
         blue = ColorCanvas(self,"blue",width=100,height=100)
+        blue.bind('<ButtonRelease-1>',self.mouse_click)        
         blue.grid(row=0, column=2)
-        blue.state = ColorCanvas.ON
+        Window.set_select_convas(red)
+        select_canvas = Window.get_select_convas()
+        print(select_canvas.rec_color)
+
+
     def mouse_click(self,event):
-        print(event)
-        # event.widget.delete()
-        event.widget.create_rectangle(10,10,60,60,fill='white')
-        event.widget.create_rectangle(20,20,50,50,fill='red')
-        # event.widget.update()
+        Window.set_select_convas(event.widget)
+
 def main():
     window = Window()
     window.title("RGBLED 顏色控制")
     window.mainloop()
-
 if __name__ == "__main__":
     main()
